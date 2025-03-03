342639698a79c9e79e6c76eb1ead151dc35a8d54
adgao <96456314+anndgao@users.noreply.github.com>
2025-03-02 21:54:48 -0500

    Added some basic function info

--- src/export.rs
+++ src/export.rs
@@ -1,7 +1,25 @@
 use std::path::PathBuf;
 
-pub struct Framebuffer {}
+pub struct Framebuffer {
+    width: u32,
+    height: u32, 
+}
 
 pub fn export_image(fb: Framebuffer, path: impl Into<PathBuf>) -> image::ImageResult<()> {
-    todo!()
+    let width = fb.width;
+    let height = fb.height;
+    let mut img = RgbImage::new(width, height);
+
+
+
+
+    return img;
+}
+
+pub fn pixel_mut(&self, x: usize, y: usize) -> &AtomicU32 {
+
+}
+
+fn into_image(self) -> Image {
+
 }
