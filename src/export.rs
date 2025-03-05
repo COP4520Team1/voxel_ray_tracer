a4c797e6d553561a3e447cfa14b1f2739bf93bf9
adgao <96456314+anndgao@users.noreply.github.com>
2025-03-04 23:33:27 -0500

    Added more stuff to functions

--- src/export.rs
+++ src/export.rs
@@ -2,24 +2,29 @@ use std::path::PathBuf;
 
 pub struct Framebuffer {
     width: u32,
-    height: u32, 
+    height: u32,
+    // vector of all the pixels
+    pixels: Box<[AtomicU32]>,
 }
-
-pub fn export_image(fb: Framebuffer, path: impl Into<PathBuf>) -> image::ImageResult<()> {
-    let width = fb.width;
-    let height = fb.height;
-    let mut img = RgbImage::new(width, height);
-
-
-
-
-    return img;
-}
-
-pub fn pixel_mut(&self, x: usize, y: usize) -> &AtomicU32 {
-
-}
-
-fn into_image(self) -> Image {
-
+impl Framebuffer {
+    pub fn export_image(fb: Framebuffer, path: impl Into<PathBuf>) -> image::ImageResult<()> {
+        // Create image from framebuffer
+        let mut img = RgbImage::new(fb.width, fb.height);
+    
+        // Copy framebuffer into image
+        //
+        //
+        //
+    
+        return img;
+    }
+    
+    pub fn pixel_mut(&self, x: usize, y: usize) -> &AtomicU32 {
+        return self.pixels[y * self.width + x];
+    
+    }
+    
+    fn into_image(self) -> Image {
+        todo!();
+    }    
 }
