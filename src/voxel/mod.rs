b3eff5f522fad8d451bcd9186709fdcb2ea5f7fd
n8banks <nathan@akhalls.com>
2025-03-07 01:26:00 -0500

    initial commit for octree functionality

--- src/voxel/mod.rs
+++ src/voxel/mod.rs
@@ -4,10 +4,32 @@ use glam::{IVec3, U8Vec3};
 #[derive(Clone, Copy, PartialEq, Eq)]
 pub struct Voxel {
     pub color: U8Vec3,
+    pub pos: IVec3,
+}
+
+pub struct Node {
+    pub vox: Voxel,
+    pub children: [Option<Box<Node>>; 8],
+}
+
+/// Node is what stores the voxel and its children
+/// 
+impl Node {
+    fn new(vox: Voxel) -> Self {
+        Self {children: [None, None, None, None], vox}
+    }
+    fn insert(&mut self, index: usize, vox: Voxel)
+    {
+        if index < 8 {
+            self.children[index] = Some(Box::new(Node::new(vox)));
+        }
+    }
 }
 
 /// An iterator that produces voxels.
-pub struct VoxelGenerator {}
+pub struct VoxelGenerator {
+    
+}
 
 impl VoxelGenerator {
     /// Create a new voxel generator.
