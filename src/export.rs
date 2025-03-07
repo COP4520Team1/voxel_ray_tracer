3b48dace7be8ff5d7d9dd74abd30fa38368143a4
adgao <96456314+anndgao@users.noreply.github.com>
2025-03-07 03:35:24 -0500

    Added to into_image fx

--- src/export.rs
+++ src/export.rs
@@ -1,21 +1,20 @@
 use std::path::PathBuf;
 
 pub struct Framebuffer {
-    width: usize,
-    height: usize,
+    width: u32,
+    height: u32,
     pixels: Box<[AtomicU32]>,
 }
 impl Framebuffer {
     // creates a vector of size width * height, converts it to box
-    pub fn new(width: usize, height: usize) -> Self {
+    pub fn new(width: u32, height: u32) -> Self {
         let pixels = (0..(width * height))
         .map(|_| AtomicU32::new(0))
         .collect::<Vec<_>>()
         .into_boxed_slice();
         Self { width, height, pixels }
-
     }
-    pub fn export_image(fb: Framebuffer, path: impl Into<PathBuf>) -> image::ImageResult<()> {
+    pub fn export_image(fb: Framebuffer /*, path: impl Into<PathBuf> */) -> image::ImageResult<()> {
         // Create image from framebuffer
         let mut img = RgbImage::new(fb.width, fb.height);
     
@@ -23,17 +22,26 @@ impl Framebuffer {
         //
         //
         //
-    
-        return img;
+        for (x, y, pixel) in img.enumerate_pixels_mut() {
+            let r = (0.3 * x as f32) as u8;
+            let b = (0.3 * y as f32) as u8;
+            *pixel = image::Rgb([r, 0, b]);
+        }
+        // Save img to file path
+        //
+        return Ok(());
     }
     
-    pub fn pixel_mut(&self, x: usize, y: usize) -> &AtomicU32 {
-        // return self.pixels[y * self.width + x];
-        todo!();
+    pub fn pixel_mut(&self, x: u32, y: u32) -> &AtomicU32 {
+        let index = (y * self.width + x) as usize;
+        return &(self.pixels[index]);
+        //todo!();
 
     }
     
-    fn into_image(self) -> Image {
-        todo!();
-    }    
+    fn into_image(self) -> ImageBuffer<()> {
+        Framebuffer::export_image(self);
+        return RgbImage::new(self.width, self.height);
+
+    }  
 }
