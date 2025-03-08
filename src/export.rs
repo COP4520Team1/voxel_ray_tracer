7278e2de5d18baaa2dab1f5f44d61f9b7ab9f6b5
adgao <96456314+anndgao@users.noreply.github.com>
2025-03-07 22:32:00 -0500

    Changed export_image function

--- src/export.rs
+++ src/export.rs
@@ -1,9 +1,28 @@
 use std::path::PathBuf;
+use image::{RgbImage, Rgb};
+use std::sync::atomic::AtomicU32;
 
-pub struct Framebuffer {}
+pub struct Framebuffer {
+    width: u32,
+    height: u32,
+    pixels: Box<[AtomicU32]>,
+}
 
 pub fn export_image(fb: Framebuffer, path: impl Into<PathBuf>) -> image::ImageResult<()> {
-    todo!()
+    let mut img = RgbImage::new(fb.width, fb.height);
+
+    // Copy pixel at x, y from Framebuffer into image
+    for x in fb.width {
+        for y in fb.height {
+            // Get RGB value from pixels 
+            // let p: [u32, 3] = fb.pixels[y * x + x];
+            img.put_pixel(x, y, Rgb([0, 0, 0]));
+        }
+    }
+    
+    img.save("render.png").unwrap();
+
+    Ok(())
 }
 
 pub struct FramebufferIter<'fb> {
@@ -15,6 +34,7 @@ impl<'fb> Iterator for FramebufferIter<'fb> {
     type Item = (); // TODO: change to AtomicU32
 
     fn next(&mut self) -> Option<Self::Item> {
-        todo!()
+        todo()!;
     }
 }
+
