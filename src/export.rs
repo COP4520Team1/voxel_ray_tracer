d246c0eb6f15ce66e3c8d67812d6e7ff15469f8e
adgao <96456314+anndgao@users.noreply.github.com>
2025-03-20 00:14:04 -0400

    Fixed export function

--- src/export.rs
+++ src/export.rs
@@ -3,26 +3,36 @@ use image::{RgbImage, Rgb};
 use std::sync::atomic::AtomicU32;
 
 pub struct Framebuffer {
-    width: u32,
-    height: u32,
+    width: usize,
+    height: usize,
     pixels: Box<[AtomicU32]>,
 }
-
-pub fn export_image(fb: Framebuffer, path: impl Into<PathBuf>) -> image::ImageResult<()> {
-    let mut img = RgbImage::new(fb.width, fb.height);
-
-    // Copy pixel at x, y from Framebuffer into image
-    for x in fb.width {
-        for y in fb.height {
-            // Get RGB value from pixels 
-            // let p: [u32, 3] = fb.pixels[y * x + x];
-            img.put_pixel(x, y, Rgb([0, 0, 0]));
-        }
+impl Framebuffer {
+    pub fn new(width: usize, height: usize) -> Framebuffer {
+        let size = width * height;
+        let pixels = (0..size).map(|_| AtomicU32::new(0)).collect::<Vec<_>>().into_boxed_slice();
+        
+        Self { width, height, pixels }
     }
+    pub fn pixel_mut(&self, x: usize, y: usize) -> &AtomicU32 {
+        let index = y * self.width + x;
+        &self.pixels[index]
+    }
+    pub fn export_image(fb: Framebuffer, path: impl Into<PathBuf>) -> image::ImageResult<()> {
+        let mut img = RgbImage::new(fb.width, fb.height);
     
-    img.save("render.png").unwrap();
-
-    Ok(())
+        // Copy pixel at x, y from Framebuffer into image
+        for x in fb.width {
+            for y in fb.height {
+                // Get RGB value from pixels 
+                img.put_pixel(x, y, Rgb(fb.pixel_mut(x, y).load()));
+            }
+        }
+        
+        img.save("render.png").unwrap();
+    
+        Ok(())
+    }
 }
 
 pub struct FramebufferIter<'fb> {
