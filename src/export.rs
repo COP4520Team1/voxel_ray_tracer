35c9062251cb8f0b33b47ded3aede0a62a926b01
adgao <96456314+anndgao@users.noreply.github.com>
2025-03-20 18:59:01 -0400

    Fixed formatting

--- src/export.rs
+++ src/export.rs
@@ -1,5 +1,5 @@
+use image::{Rgba, RgbaImage};
 use std::path::PathBuf;
-use image::{RgbaImage, Rgba};
 use std::sync::atomic::AtomicU32;
 use std::sync::atomic::Ordering;
 
@@ -11,15 +11,21 @@ pub struct Framebuffer {
 impl Framebuffer {
     pub fn new(width: usize, height: usize) -> Framebuffer {
         let size = width * height;
-        let pixels = (0..size).map(|_| AtomicU32::new(0)).collect::<Vec<_>>().into_boxed_slice();
-        
-        Self { width, height, pixels }
+        let pixels = (0..size)
+            .map(|_| AtomicU32::new(0))
+            .collect::<Vec<_>>()
+            .into_boxed_slice();
+
+        Self {
+            width,
+            height,
+            pixels,
+        }
     }
     pub fn pixel_mut(&self, x: usize, y: usize) -> &AtomicU32 {
         let index = y * self.width + x;
         &self.pixels[index]
     }
-    
 }
 pub fn export_image(fb: Framebuffer, path: impl Into<PathBuf>) -> image::ImageResult<()> {
     let mut img = RgbaImage::new(fb.width as u32, fb.height as u32);
@@ -32,10 +38,14 @@ pub fn export_image(fb: Framebuffer, path: impl Into<PathBuf>) -> image::ImageRe
             let g_byte = ((current_pixel & 0x00FF0000) >> 16) as u8;
             let b_byte = ((current_pixel & 0x0000FF00) >> 8) as u8;
             let a_byte = (current_pixel & 0x000000FF) as u8;
-            img.put_pixel(x.try_into().unwrap(), y.try_into().unwrap(), Rgba([r_byte, g_byte, b_byte, a_byte]));
+            img.put_pixel(
+                x.try_into().unwrap(),
+                y.try_into().unwrap(),
+                Rgba([r_byte, g_byte, b_byte, a_byte]),
+            );
         }
     }
-    
+
     img.save("render.png").unwrap();
 
     Ok(())
@@ -52,4 +62,3 @@ impl<'fb> Iterator for FramebufferIter<'fb> {
         todo!();
     }
 }
-
