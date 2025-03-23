966f64c9658f5df01b4e0f208860221ee2f8f99d
Jonah Henriksson <33059163+JonahPlusPlus@users.noreply.github.com>
2025-03-23 18:22:55 -0400

    Implemented parallel rendering (#23)
    * Implemented parallel rendering
    
    * Update mod.rs

--- src/export.rs
+++ src/export.rs
@@ -1,5 +1,10 @@
 use image::{Rgba, RgbaImage};
-use std::path::PathBuf;
+use rayon::iter::plumbing::bridge;
+use rayon::iter::plumbing::Producer;
+use rayon::iter::IndexedParallelIterator;
+use rayon::iter::IntoParallelIterator;
+use rayon::iter::ParallelIterator;
+use std::path::Path;
 use std::sync::atomic::AtomicU32;
 use std::sync::atomic::Ordering;
 
@@ -22,16 +27,27 @@ impl Framebuffer {
             pixels,
         }
     }
+
     pub fn pixel_mut(&self, x: usize, y: usize) -> &AtomicU32 {
         let index = y * self.width + x;
         &self.pixels[index]
     }
 }
-pub fn export_image(fb: Framebuffer, path: impl Into<PathBuf>) -> image::ImageResult<()> {
+
+impl<'b> IntoParallelIterator for &'b Framebuffer {
+    type Iter = ParIter<'b>;
+    type Item = PixelRef<'b>;
+
+    fn into_par_iter(self) -> Self::Iter {
+        ParIter { buffer: self }
+    }
+}
+
+pub fn export_image(fb: Framebuffer, path: impl AsRef<Path>) -> image::ImageResult<()> {
     let mut img = RgbaImage::new(fb.width as u32, fb.height as u32);
     // Copy pixel at x, y from Framebuffer into image
-    for x in (0..fb.width) {
-        for y in (0..fb.height) {
+    for x in 0..fb.width {
+        for y in 0..fb.height {
             // Get RGBA value from pixels - first byte is R, second is G, etc
             let current_pixel = fb.pixel_mut(x, y).load(Ordering::Relaxed);
             let r_byte = (current_pixel >> 24) as u8;
@@ -46,19 +62,147 @@ pub fn export_image(fb: Framebuffer, path: impl Into<PathBuf>) -> image::ImageRe
         }
     }
 
-    img.save("render.png").unwrap();
+    img.save(path).unwrap();
 
     Ok(())
 }
-pub struct FramebufferIter<'fb> {
-    fb: &'fb Framebuffer,
-    i: usize,
+
+pub struct Iter<'b> {
+    buffer: &'b Framebuffer,
+    start: usize,
+    end: usize,
 }
 
-impl<'fb> Iterator for FramebufferIter<'fb> {
-    type Item = (); // TODO: change to AtomicU32
+impl<'b> Iterator for Iter<'b> {
+    type Item = PixelRef<'b>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        todo!();
+        if self.start >= self.end {
+            return None;
+        }
+
+        let color = self.buffer.pixels.get(self.start)?;
+
+        let pixel = PixelRef {
+            x: self.start % self.buffer.width,
+            y: self.start / self.buffer.width,
+            value: color,
+        };
+
+        // increment for next iteration
+        self.start += 1;
+
+        Some(pixel)
     }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let len: usize = self.end - self.start;
+        (len, Some(len))
+    }
+}
+
+impl<'b> DoubleEndedIterator for Iter<'b> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        if self.start >= self.end {
+            return None;
+        }
+
+        // decrement first since end is exclusive
+        self.end -= 1;
+
+        let color = self.buffer.pixels.get(self.end)?;
+
+        let pixel = PixelRef {
+            x: self.end % self.buffer.width,
+            y: self.end / self.buffer.width,
+            value: color,
+        };
+
+        Some(pixel)
+    }
+}
+
+impl<'b> ExactSizeIterator for Iter<'b> {}
+
+pub struct ParIter<'b> {
+    buffer: &'b Framebuffer,
+}
+
+impl<'b> ParallelIterator for ParIter<'b> {
+    type Item = PixelRef<'b>;
+
+    fn drive_unindexed<C>(self, consumer: C) -> C::Result
+    where
+        C: rayon::iter::plumbing::UnindexedConsumer<Self::Item>,
+    {
+        bridge(self, consumer)
+    }
+
+    fn opt_len(&self) -> Option<usize> {
+        Some(self.len())
+    }
+}
+
+impl<'b> IndexedParallelIterator for ParIter<'b> {
+    fn len(&self) -> usize {
+        self.buffer.pixels.len()
+    }
+
+    fn drive<C: rayon::iter::plumbing::Consumer<Self::Item>>(self, consumer: C) -> C::Result {
+        bridge(self, consumer)
+    }
+
+    fn with_producer<CB: rayon::iter::plumbing::ProducerCallback<Self::Item>>(
+        self,
+        callback: CB,
+    ) -> CB::Output {
+        callback.callback(ParIterProducer {
+            buffer: &self.buffer,
+            start: 0,
+            end: self.len(),
+        })
+    }
+}
+
+struct ParIterProducer<'b> {
+    buffer: &'b Framebuffer,
+    start: usize,
+    end: usize,
+}
+
+impl<'b> Producer for ParIterProducer<'b> {
+    type Item = PixelRef<'b>;
+    type IntoIter = Iter<'b>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        Iter {
+            buffer: self.buffer,
+            start: self.start,
+            end: self.end,
+        }
+    }
+
+    fn split_at(self, index: usize) -> (Self, Self) {
+        let mid = self.start + index;
+        assert!(self.start < mid && mid < self.end, "Out of range!");
+
+        (
+            Self {
+                buffer: self.buffer,
+                start: self.start,
+                end: mid,
+            },
+            Self {
+                buffer: self.buffer,
+                start: mid,
+                end: self.end,
+            },
+        )
+    }
+}
+
+pub struct PixelRef<'b> {
+    pub x: usize,
+    pub y: usize,
+    pub value: &'b AtomicU32,
 }
